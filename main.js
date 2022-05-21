fdaa1f7c098b4b253122a9a01c46c9575a315cd9
newVerstka <rolingstone@yandex.ru>
2022-05-21 20:05:14 +0700

    Add calculator
    Калькулятор

---
+++ main.js
@@ -0,0 +1,101 @@
+const display = document.querySelector('.display');
+const op = document.querySelectorAll('.operator');
+const numberEl = document.querySelectorAll('.num');
+
+let num1 = '';
+let num2 = '';
+let num3 = '';
+let result = false;
+let sign = '';
+let action = ['*', '+', '-', '/'];
+let memory = false;
+let memoryNum = '';
+let localMemory = 0;
+
+function clearAll() {
+      num1 = '';
+      num2 = '';
+      num3 = '';
+      result = false;
+      sign = '';
+      display.textContent = '0';
+}
+
+function del() {
+      if(num1 !=='' && sign === ''){
+            num1 = display.textContent = display.textContent.slice(0, -1);
+      } else if(num1 !=='' && sign !==''){
+            num2 = display.textContent = display.textContent.slice(0, -1);
+      }
+      return;
+}
+
+document.querySelector('.del').addEventListener('click', del);
+
+function percent() {
+      num1 = (num1 * num2) / 100;
+      result = true;
+      display.textContent = num1;
+}
+
+for (let i = 0; i < numberEl.length; i++) {
+      numberEl[i].addEventListener('click', function (e) {
+            let number = e.target.textContent;
+
+            if (num2 === '' && sign === '') {
+                  num1 += number;
+                  
+                  display.textContent = num1;
+            } else if (num1 !== '' && num2 !== '' && sign !== '' && result) {
+                  num2 = number;
+                  result = false;
+                  display.textContent = num2;
+            }
+            else {
+                  num2 += number;
+                  
+                  display.textContent = num2;
+            }
+            return;
+      });
+}
+
+for (let i = 0; i < op.length; i++) {
+      op[i].addEventListener('click', function (e) {
+            let operator = e.target.textContent;
+            if (action.includes(operator)) {
+                  sign = operator;
+            }
+            if (operator === '=') {
+                  switch (sign) {
+                        case '+':
+                              num1 = (+num1) + (+num2);
+                              break;
+                        case '-':
+                              num1 = num1 - num2;
+                              break;
+                        case '*':
+                              num1 = num1 * num2;
+                              break;
+                        case '/':
+                              if (num2 === '0') {
+                                    num1 = '';
+                                    num2 = '';
+                                    sign = '';
+                                    display.textContent = 'Ошибка';
+
+                                    return;
+                              } else {
+                                    num1 = num1 / num2;
+                              }
+                              break;
+                  }
+                  result = true;
+                  display.textContent = num1;
+
+            }
+
+
+
+      });
+}
